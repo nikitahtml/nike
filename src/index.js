79e4c4edc7575407f46679eb2c3728bcc6ff6275
nikitahtml <114980124+nikitahtml@users.noreply.github.com>
2023-10-22 21:50:14 +0600

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.css';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import App from './App';
 
-import CRUD from './CRUD.js';
-
-
-const  root = ReactDOM.createRoot(document.getElementById('root'));
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-  
   <React.StrictMode>
-     <CRUD /> 
-  </React.StrictMode>  
-  );  
-  // root.render(<CRUD />)
+    <App />
+  </React.StrictMode>
+);
